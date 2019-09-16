49c5990459f000d020d1307ca32b02c6e20165df
asbyth <cyronize@gmail.com>
2019-09-16 08:56:07 -0400

    port to 1.7

--- src/main/java/club/sk1er/optifinecapes/CapeEditor.java
+++ src/main/java/club/sk1er/optifinecapes/CapeEditor.java
@@ -1,14 +1,14 @@
 package club.sk1er.optifinecapes;
 
 import club.sk1er.optifinecapes.gui.GuiScreenCapeOF;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiOptions;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = "cape_editor", name = "Optifine Cape Editor", version = "1.0")
 public class CapeEditor {
@@ -23,7 +23,8 @@ public class CapeEditor {
         if (event.gui instanceof GuiOptions) {
             GuiButton optifineCape;
 
-            event.buttonList.add(optifineCape = new GuiButton(62727568, 2, event.gui.height - 20, 100, 20, "Optifine Cape"));
+            event.buttonList.add(optifineCape = new GuiButton(62727568, 2,
+                    event.gui.height - 20, 100, 20, "Optifine Cape"));
 
             if (Minecraft.getMinecraft().theWorld == null) {
                 optifineCape.visible = false;
